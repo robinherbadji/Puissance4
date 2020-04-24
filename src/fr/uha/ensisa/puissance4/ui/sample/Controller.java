c6c36c98a6d2005c346f90058ff5e28ab2a0d806
yassine.sameh <yassine.sameh@sfr.fr>
2020-04-24 14:08:54 +0200

    Controller ui added

---
+++ src/fr/uha/ensisa/puissance4/ui/sample/Controller.java
@@ -0,0 +1,17 @@
+package fr.uha.ensisa.puissance4.ui.sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void handleStartButton(ActionEvent actionEvent) {
+    }
+
+    public void handleButtonClose(ActionEvent actionEvent) {
+    }
+
+    public void handleButton(ActionEvent actionEvent) {
+    }
+
+    public void handleCheckButton(ActionEvent actionEvent) {
+    }
+}
